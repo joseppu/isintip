4160a773cffd59d49bb00b79f85bd8ebc5f05d04
Yusuf Yagci <yusufyagci22@gmail.com>
2022-06-18 13:51:52 +0800

    noreferrer to ahref

--- components/TrialSizeSample.tsx
+++ components/TrialSizeSample.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 const TrialSizeSample = () => {
   return (
     <div className="max-w-5xl p-10 mx-auto">
-    <a href="https://www.scbt.com/whats-new/antibody-exchange-program" target="_blank">
+    <a href="https://www.scbt.com/whats-new/antibody-exchange-program" target="_blank" rel="noreferrer">
     <img src="/trial-size-sample.jpg" alt="trial size sample" />
     </a>
   </div>
